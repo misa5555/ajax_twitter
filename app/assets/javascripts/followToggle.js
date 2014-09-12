eba26c4386ba02827aa95f68a721f861531b8769
App Academy Student <student@appacademy.io>
2014-09-12 15:57:13 -0700

    follow/unfollow button working

--- app/assets/javascripts/followToggle.js
+++ app/assets/javascripts/followToggle.js
@@ -3,14 +3,60 @@ $.FollowToggle = function (el) {
 	this.userId = this.$el.data("user-id");
 	this.followState = this.$el.data("initial-follow-state");
 	this.render();
+	this.$el.on('click', this.handleClick.bind(this));
 };
 
 $.FollowToggle.prototype.render = function () {
+	if (this.followState === "followed" || this.followState === "unfollowed") {
+		this.$el.prop("disabled", false);
+	} else if (this.followState === "followed" || this.followState === "unfollowed"){
+		this.$el.prop("disabled", true);
+	}
+	 
   if (this.followState === "followed") {
-  	this.$el.text("Unfollowed");
+  	this.$el.text("Unfollow");
   } else if (this.followState === "unfollowed") {
   	this.$el.text("Follow!");
-  }
+  } else if (this.followState === "following") {
+  	this.$el.text("Following");
+	} else if (this.followState === "unfollowing") {
+		this.$el.text("Unfollowing");
+	}
+};
+
+$.FollowToggle.prototype.handleClick = function (event) {
+	var that = this;
+	event.preventDefault();
+	
+	if (that.followState === "followed") {
+		that.followState = "unfollowing"
+	} else {
+		that.followState = "following" 
+	}
+	that.render();
+	
+	if (that.followState === "unfollowing") {
+		console.log(that.userId +"/follow");
+		$.ajax({
+			url: "/users/" + that.userId + "/follow",
+			type: 'DELETE', 
+			dataType: 'json',
+			success: function () {
+				that.followState = "unfollowed" 
+				that.render();
+			}
+		});
+	} else if (that.followState === "following") {
+		
+		$.ajax({
+			url: "/users/"+that.userId +"/follow",
+			type: 'POST', 
+			success: function () {
+				that.followState = "followed"
+				that.render();
+			}
+		})
+	}
 };
 
 $.fn.followToggle = function () {
